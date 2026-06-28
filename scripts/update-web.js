44d448b481edc2f42928fdd56473605c5de25291
Will Zhang <32278614+WillCZhang@users.noreply.github.com>
2026-06-28 12:24:39 -0700

    test integ flow

---
+++ scripts/update-web.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+const issueTitle = process.env.ISSUE_TITLE;
+const issueBody = process.env.ISSUE_BODY;
+
+console.log(`Running web update with the following issue
+$ISSUE_TITLE=${issueTitle}
+$ISSUE_BODY=${issueBody}
+`);
+
+const parsedStudentId = issueTitle.split('|')[0];
+const parsedStudentName = issueTitle.split('|')[1];
+const parsedCourseTitle = issueTitle.split('|')[2];
+
+const updatedContent = fs.readFileSync('template.html', 'utf8')
+  .replace('[STUDENT_NAME_PLACEHOLDER]', parsedStudentName)
+  .replace('[COURSE_TITLE_PLACEHOLDER]', parsedCourseTitle);
+
+fs.writeFileSync('student_websites/' + parsedStudentId + ".html", updatedContent);
