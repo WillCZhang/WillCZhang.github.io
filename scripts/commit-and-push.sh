8224e0df8cc647784c875eb753c673991e993e77
Will Zhang <32278614+WillCZhang@users.noreply.github.com>
2026-06-28 14:03:30 -0700

    update workflow

---
+++ scripts/commit-and-push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+
+git add .
+
+git diff-index --quiet HEAD || git commit -m "chore: update static asset from issue #${{ github.event.issue.number }}"
+
+MAX_SLEEP=10
+
+echo "Starting Git sync loop..."
+
+while true; do
+    echo "Attempting to pull and push..."
+    
+    # Execute pull with rebase, and if successful, execute push
+    if git pull --rebase && git push; then
+        echo "Success: Git repository is up to date and pushed!"
+        break
+    else
+        SLEEP_TIME=$(( RANDOM % MAX_SLEEP + 1 ))
+        
+        echo "Command failed. Retrying in $SLEEP_TIME seconds..."
+        sleep "$SLEEP_TIME"
+    fi
+done
